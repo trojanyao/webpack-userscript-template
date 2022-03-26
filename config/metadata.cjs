2d4c9ab9316a4a3f3838cba276f7c6258250bfa5
Trojan Yao <ytj1996@sina.com>
2022-03-26 17:42:30 +0800

    refactor: 修改配置

--- config/metadata.cjs
+++ config/metadata.cjs
@@ -2,7 +2,7 @@ const { author, dependencies, repository, version } = require('../package.json')
 
 module.exports = {
   name: 'webpack-userscript-template',
-  namespace: 'https://trim21.me/',
+  namespace: '',
   version: version,
   author: author,
   source: repository.url,
